65730628b688c8db851a450f7ede937e2d2dae65
paul.pham <paul.pham@bitkub.com>
2024-07-20 19:13:24 +0700

    FE login okta

---
+++ Downloads/okta-hosted-login-dev/dev.sh
@@ -0,0 +1,5 @@
+export CLIENT_ID="0oagllbqkndAe2ArM5d7"
+export ISSUER="https://dev-96634040.okta.com/oauth2/default"
+export CLIENT_SECRET="QG10g2WJuee00WBYxeaa0Bpetbxu3mDr89u1eMvuOjy7apqVHHSGQytEXMOQCOAN"
+
+go run main.go
